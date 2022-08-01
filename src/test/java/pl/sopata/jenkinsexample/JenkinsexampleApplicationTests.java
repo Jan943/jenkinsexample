1c2dd6e5d3df5164508f4783f299428025b6588b
Jan943 <jansopata10@gmail.com>
2022-08-02 00:23:16 +0200

    Initial commit

---
+++ src/test/java/pl/sopata/jenkinsexample/JenkinsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sopata.jenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsexampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
